4690e059ec976015ef08597f30a6d5de8b0eac86
DESKTOP-0H7CPIA\admin <nwethazin@99.co>
2021-08-15 13:26:40 +0800

    chore (Fake Task Repository) : Use the Fake Repository inside the viewmodel

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,6 +3,8 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
@@ -21,16 +23,22 @@ class TasksViewModelTest {
 
     private lateinit var tasksViewModel: TasksViewModel
 
+    // use a fake repository to be injected into the view model
+    private lateinit var tasksRepository: FakeTestRepository
+
     @Before
     fun setupViewModel() {
-        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        tasksRepository = FakeTestRepository()
+        val task1 = Task("Title1","Description1")
+        val task2 = Task("Title2","Description2",true)
+        val task3 = Task("Title3","Description3",true)
+        tasksRepository.addTasks(task1,task2,task3)
+
+        tasksViewModel = TasksViewModel(tasksRepository)
     }
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
-        // given a fresh TasksViewModel
-        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-
         // when adding a new task
         tasksViewModel.addNewTask()
 
@@ -44,9 +52,6 @@ class TasksViewModelTest {
 
     @Test
     fun setFilterAllTasks_tasksAndViewVisible() {
-        // given a fresh TasksViewModel
-        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-
         // when filtering type is ALL_TASKS
         tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
 
